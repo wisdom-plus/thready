e96b19869f951140e6518077da7a9119eb0879a0
wisdom-plus <tomoron326@outlook.jp>
2025-06-03 08:17:14 +0900

    add:init users table

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,14 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 CREATE TABLE threads (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE messages (
